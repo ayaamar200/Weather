6e11f3d1ddc250505b8ad931ffb065c8a545c1e8
ayaamar2001 <129336185+ayaamar200@users.noreply.github.com>
2024-12-14 21:14:40 +0200

    updated

--- js/index.js
+++ js/index.js
@@ -3,26 +3,9 @@
 let searchInput = document.getElementById("search");
 let btnFind = document.getElementById("btnFind");
 
-// *********** Today Details ***********
-let todayName = document.getElementById("todayName");
-let todayNumber = document.getElementById("todayNumber");
-let todayMonth = document.getElementById("todayMonth");
-let todayLocation = document.getElementById("todayLocation");
-let todayTemp = document.getElementById("todayTemp");
-let todayWeatherIcon = document.getElementById("todayWeatherIcon");
-let todayDesc = document.getElementById("todayDesc");
-
-// *********** Next Day Details ***********
-let nextDayName = document.querySelectorAll("#nextDayName");
-let nextDayLocation = document.querySelectorAll("#nextDayLocation");
-let nextDayMaxTemp = document.querySelectorAll("#nextDayMaxTemp");
-let nextDayMinTemp = document.querySelectorAll("#nextDayMinTemp");
-let nextDayDesc = document.querySelectorAll("#nextDayDesc");
-let nextDayWeatherIcon = document.querySelectorAll("#nextDayIcon");
 
 // ! ***************************** Global Functions *****************************
 
-startApp();
 async function startApp(cityName = "Cairo") {
   let weatherData = await getWeatherData(cityName);
   if (!weatherData.error) {
@@ -31,51 +14,136 @@ async function startApp(cityName = "Cairo") {
   }
 }
 
+startApp();
+
 async function getWeatherData(cityName) {
-  let responseData = await fetch(
+  let response = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=0298ebed4d51410f89f175639240612&q=${cityName}&days=3`
   );
-  let weatherData = await responseData.json();
-  // console.log(weatherData);
-  return weatherData;
+
+  if (response.ok) {
+    let weatherData = await response.json();
+    // console.log(weatherData);
+    return weatherData;
+  }
 }
 
 function displayTodayWeather(weatherData) {
   let todayDate = new Date();
-  todayName.innerHTML = todayDate.toLocaleDateString("en-US", {
-    weekday: "long",
-  });
-  todayMonth.innerHTML = todayDate.toLocaleDateString("en-US", {
+
+  let todayName = todayDate.toLocaleDateString("en-US", { weekday: "long" });
+  let todayMonth = todayDate.toLocaleDateString("en-US", {
     month: "long",
   });
-  todayNumber.innerHTML = todayDate.getDate();
-  todayLocation.innerHTML = weatherData.location.name;
-  todayTemp.innerHTML = weatherData.current.temp_c + "°C";
-  todayWeatherIcon.setAttribute(
-    "src",
-    "https:" + weatherData.current.condition.icon
-  );
-  todayDesc.innerHTML = weatherData.current.condition.text;
+  let todayNumber = todayDate.getDate();
+
+  document.getElementById("rowData").innerHTML = `
+          <div class="col-md-4 weather-col">
+                <div class="today weather">
+                  <div
+                    class="weather-header d-flex justify-content-between align-items-center">
+                    <div id="todayName">${todayName}</div>
+                    <div>
+                      <span id="todayNumber">${todayNumber}</span>
+                      <span id="todayMonth">${todayMonth}</span>
+                    </div>
+                  </div>
+
+                  <div class="weather-content" id="current">
+                    <div class="location text-white" id="todayLocation">${weatherData.location.name}</div>
+                    <div
+                      class="degree d-flex flex-wrap justify-content-between align-items-center"
+                    >
+                      <div class="num" id="todayTemp">${weatherData.current.temp_c}°C</div>
+
+                      <div class="weather-icon">
+                        <img
+                          id="todayWeatherIcon"
+                          src="${weatherData.current.condition.icon}"
+                          alt="Weather Condition Icon"
+                          width="90"
+                        />
+                      </div>
+                    </div>
+                    <div
+                      id="todayDesc"
+                      class="custom main-color py-4"
+                    >${weatherData.current.condition.text}</div>
+
+                    <div class="d-flex gap-3">
+                      <div
+                        class="d-flex justify-content-center align-items-center gap-1"
+                      >
+                        <img
+                          src="./imgs/icon-umberella@2x.png"
+                          alt="Icon Umbrella"
+                          width="20"
+                        />
+                        <span>20%</span>
+                      </div>
+                      <div
+                        class="d-flex justify-content-center align-items-center gap-1"
+                      >
+                        <img
+                          src="./imgs/icon-wind@2x.png"
+                          alt="Icon Wind"
+                          width="20"
+                        />
+                        <span>18km/h</span>
+                      </div>
+                      <div
+                        class="d-flex justify-content-center align-items-center gap-1"
+                      >
+                        <img
+                          src="./imgs/icon-compass@2x.png"
+                          alt="Icon Compass"
+                          width="20"
+                        />
+                        <span>East</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+          </div>
+  `;
 }
 
 function displayTomorrowWeather(weatherData) {
   let tomorrowData = weatherData.forecast.forecastday;
-  // console.log(tomorrowData);
+  let container = "";
 
   for (let i = 0; i < 2; i++) {
     let nextDate = new Date(tomorrowData[i + 1].date);
-
-    nextDayName[i].innerHTML = nextDate.toLocaleDateString("en-US", {
-      weekday: "long",
-    });
-    nextDayMaxTemp[i].innerHTML = tomorrowData[i + 1].day.maxtemp_c + "°C";
-    nextDayMinTemp[i].innerHTML = tomorrowData[i + 1].day.mintemp_c + "°C";
-    nextDayDesc[i].innerHTML = tomorrowData[i + 1].day.condition.text;
-    nextDayWeatherIcon[i].setAttribute(
-      "src",
-      "https:" + tomorrowData[i + 1].day.condition.icon
-    );
+    let nextDayName = nextDate.toLocaleDateString("en-US", { weekday: "long" });
+    container += `
+      <div class="col-md-4 weather-col">
+        <div class="weather">
+            <div class="weather-header text-center">
+              <div id="nextDayName">${nextDayName}</div>
+            </div>
+            <div class="weather-content">
+              <div class="weather-icon">
+                <img src="${
+                  tomorrowData[i + 1].day.condition.icon
+                }" alt="Weather Condition Icon" width="48" id="nextDayIcon" />
+              </div>
+              <div class="degree" id="nextDayMaxTemp">${
+                tomorrowData[i + 1].day.maxtemp_c
+              }°C</div>
+              <span id="nextDayMinTemp">${
+                tomorrowData[i + 1].day.mintemp_c
+              }°C</span>
+              <div
+                class="custom main-color py-4"
+                id="nextDayDesc"
+              >${tomorrowData[i + 1].day.condition.text}</div>
+            </div>
+        </div>
+      </div>
+    `;
   }
+
+  document.getElementById("rowData").innerHTML += container;
 }
 
 // *********** Search Events ***********
@@ -90,4 +158,3 @@ btnFind.addEventListener("click", function () {
   startApp(cityName);
   // console.log(searchValue);
 });
-
